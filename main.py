5d411846d7ffc64c96918c0f3539800786f97869
Jerome <jeromeparent9@gmail.com>
2021-02-27 15:55:07 -0500

    rename main

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 class Fixed:
     """Type arithmétique à point fixe."""
+
     def __init__(self, n):
         self.n = n
-        # Partie entiere
-        self.e = str(n).split('.')[0]
-        self.eBinaire = bin(int(self.e))
-        # Partie fractionnaire
-        try:
-            self.f = str(n).split('.')[1]
-            self.fBinaire = bin(int(self.f))      
-        except:
-            pass
+        multiplier = ''
+
+        if '.' in str(n):
+            self.point_index = str(n).index('.')
+            number_of_zeroes = len(str(n).replace('.', '')) - self.point_index
+            multiplier = '1' + multiplier.zfill(number_of_zeroes)
+            n = int(n * int(multiplier))
+            self.multd = n
+            self.bin = bin(n)
+        else:
+            self.bin = bin(int(n))
 
     def __add__(self, other):
-        if type(self.n == int and type(other.n == int)):
-            e = int(self.eBinaire, 2) + int(other.eBinaire, 2)
-            return e
-        else:
-            e = int(self.eBinaire, 2) + int(other.eBinaire, 2)
-            f = int(self.fBinaire, 2) + int(other.fBinaire, 2)
-            return Fixed(e) + Fixed(f)
-        
+        point = self.point_index if self.point_index > other.point_index else other.point_index
+
+        sum = int(self.bin, 2) + int(other.bin, 2)
+        sum = str(sum)[:point] + '.' + str(sum)[point:]
+        return float(sum)
+
     def __sub__(self, other):
         pass
 
@@ -30,9 +31,16 @@ class Fixed:
     def __truediv__(self, other):
         pass
 
-x = Fixed(12.1)
-y = Fixed(10)
+
+x = Fixed(411.1)
+y = Fixed(1231.1)
+c = Fixed(3.855)
+a = Fixed(122)
+
 sum = x + y
-print(sum)
-# print(x.e, x.f)
-# print(x.eBinaire, x.fBinaire)
+print(type(sum))
+
+# print(x.n, x.bin)
+# print(y.n, y.bin)
+# print(c.n, c.bin)
+# print(a.n, a.bin)
