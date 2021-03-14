e03ff4dec21566561c4cb60d24d7b77c54645262
Jerome <jeromeparent9@gmail.com>
2021-03-14 17:05:22 -0400

    working

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 class Fixed:
     """Type arithmétique à point fixe."""
-
     def __init__(self, n):
         self.n = n
         factor = ''
@@ -14,6 +13,7 @@ class Fixed:
             self.overflow = True if len(whole) > 12 else False
 
             factor = '1' + factor.zfill(len(split_number[1]))
+            # print(split_number[1])
             fraction = int(split_number[1]) / int(factor)
             self.fraction = f'{int(fraction / 0.0625):04b}'
 
@@ -34,7 +34,8 @@ class Fixed:
             self.bin = f'{self.whole}.{self.fraction}'
 
     def __str__(self):
-        return str(f'{self.bin} ({self.n})') if self.overflow == False else str(f'{self.bin} ({OverflowError}) ({self.n})')
+        # return str(f'{self.bin} ({self.n})') if self.overflow == False else str(f'{self.bin} ({OverflowError}) ({self.n})')
+        return str(self.n)
 
     def __add__(self, other):
         result = int(self.whole, 2) + int(other.whole, 2) + (int(self.fraction, 2) * 0.0625) + (int(other.fraction, 2) * 0.0625)
@@ -54,21 +55,30 @@ class Fixed:
 
     def __truediv__(self, other):
         factor = self.shifts + other.shifts
-
         result = (int(self.bin.replace('.', ''), 2) / int(other.bin.replace('.', ''), 2)) 
 
         return Fixed(result)
 
-x = Fixed(53)
-y = Fixed(2)
-c = x + y
-u = x * y
-v = x - y
-t = x / y
-print(f'{x} + {y} = {c}')
-print(f'{x} * {y} = {u}')
-print(f'{x} - {y} = {v}')
-print(f'{x} / {y} = {t}')
+# x = Fixed(53.5)
+# y = Fixed(2)
+# c = x + y
+# u = x * y
+# v = x - y
+# t = x / y
+# print(f'{x} + {y} = {c}')
+# print(f'{x} * {y} = {u}')
+# print(f'{x} - {y} = {v}')
+# print(f'{x} / {y} = {t}')
 
-# print(f'{x}, shifts: {x.shifts}')
-# print(f'{y}, shifts: {y.shifts}')
+def newton(xt, n):
+    xtp1 = Fixed(0.5) * (xt + (Fixed(n) / xt))
+    return Fixed(xtp1)
+
+def newton_sqrt(n, iterations):
+    xt = Fixed(0.5 * n)
+
+    for t in range(iterations):
+        xt = newton(xt, n)
+        print(xt)
+
+newton_sqrt(7, 5)
